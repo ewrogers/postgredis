4e328b90e4f1a197c629f57418d91757c90ba64d
Erik Rogers <ewrogers@gmail.com>
2025-05-24 22:49:51 -0400

    refactor: remove the frame tick

--- src/server.rs
+++ src/server.rs
@@ -2,10 +2,8 @@ use crate::client::handle_client;
 use crate::commands::ClientCommand;
 use std::collections::VecDeque;
 use std::net::SocketAddr;
-use std::time::Duration;
 use tokio::net::TcpListener;
 use tokio::sync::mpsc::{unbounded_channel, UnboundedReceiver, UnboundedSender};
-use tokio::time::interval;
 
 pub struct Server {
     listener: TcpListener,
@@ -29,8 +27,6 @@ impl Server {
     }
 
     pub async fn run(&mut self) {
-        // Frame interval for 120 FPS
-        let mut frame_interval = interval(Duration::from_micros(1_000_000 / 120));
         let mut client_command_queue: VecDeque<ClientCommand> = VecDeque::new();
 
         loop {
@@ -43,19 +39,21 @@ impl Server {
                         handle_client(stream, tx).await
                     });
                 }
-                // Tick the frame
-                _ = frame_interval.tick() => {
-                    // Drain events from the channel
-                    while let Ok(cmd) = self.client_event_rx.try_recv() {
-                        client_command_queue.push_back(cmd);
-                    }
 
-                    // Process queued client commands
-                    while let Some(cmd) = client_command_queue.pop_front() {
-                        println!("Received command: {:?}", cmd);
-                        // TODO: reply to client
+                // Receive the next command from the client queue
+                Some(resp) = self.client_event_rx.recv() => {
+                    match ClientCommand::try_from(resp) {
+                        Ok(command) => client_command_queue.push_back(command),
+                        Err(err) => {
+                            eprintln!("Client disconnected: {}", err);
+                        }
                     }
                 }
+            }
+
+            // Process the event queue
+            while let Some(command) = client_command_queue.pop_front() {
+                println!("Command: {:?}", command);
             }
         }
     }
